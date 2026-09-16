d5857a8aa3a2be000538d1fe265fea072b94accb
Kiss Máté <km.kissmate@gmail.com>
2026-09-16 19:06:08 +0200

    Added spacy lemmatizer

---
+++ data_processing/rawdata/README.md
@@ -0,0 +1,5 @@
+I used LangX's most common words lists as my starting data.
+
+The format of each language's list is the same as for english-most-common-words.csv (which I've included here for reference).
+
+To recreate my data, you can download the lists from https://langx.io/tools/most-common-words.
--- data_processing/scripts.py
+++ data_processing/scripts.py
@@ -1,4 +1,8 @@
 import csv
+import spacy
+import en_core_web_sm
+import it_core_news_sm
+
 
 def get_words_from_csv(file_path):
     """
@@ -11,9 +15,30 @@ def get_words_from_csv(file_path):
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
-            if len(row[1]) >= 3 and "'" not in row[1]:
+            if len(row[1]) >= 3 and "'" not in row[1] and "-" not in row[1]:
                     words.append(row[1])
-    print(len(words))
+    print("Total number of words: ",len(words))
     return words
 
-print(get_words_from_csv('rawdata/english-most-common-words.csv'))
+def lemmatize(wordlist, package):
+    """
+    Lemmatize a list of words using spacy.
+    :param wordlist: A list of words to lemmatize
+    :param package: The spacy package to use for lemmatization (e.g. en_core_web_sm, it_core_news_sm)
+    :return: A list of lemmatized words
+    """
+    nlp = spacy.load(package)
+    lemmatized_words = {}
+    for word in wordlist:
+        doc = nlp(word)
+        lemmatized_words[word] = doc[0].lemma_
+    # Correcting known lemmatization errors
+
+    return lemmatized_words
+
+def main():
+    wordlist = lemmatize(get_words_from_csv('data_processing/rawdata/english-most-common-words.csv'), 'en_core_web_sm')
+    print(wordlist)
+
+if __name__ == "__main__":
+    main()
